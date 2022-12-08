73cc3f46edf25dbf7c3b86fbb1416c7c6b3af134
tcmhwd <120079185+tcmhwd@users.noreply.github.com>
2022-12-08 16:40:28 +0900

    Update app.py

--- app.py
+++ app.py
@@ -31,4 +31,4 @@ def home():
     return render_template("index.html", prediction_uti='{}'.format(a1))
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=False)
